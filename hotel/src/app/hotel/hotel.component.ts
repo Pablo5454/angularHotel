7a00aaf796a022a7214d413a0c80ef00f47cd72f
Pablo Perucha <p_peruchacab@fpzornotza.com>
2024-12-08 21:05:12 +0100

    Cambios 2024/12/08

--- hotel/src/app/hotel/hotel.component.ts
+++ hotel/src/app/hotel/hotel.component.ts
@@ -9,13 +9,49 @@ import { Viajero } from '../models/viajero';
 export class HotelComponent {
   public viajeros: Array<Viajero>;
   public nuevo = false;
+  public editar = false;
 
   constructor(){
     this.viajeros = [
       new Viajero("Begoña", "Begoñez", "23232", "20/12/2023", "23/12/2023", "23"),
       new Viajero("Bigoña", "Bigoñez", "985632", "20/12/2024", "23/12/2024", "2563"),
       new Viajero("Bagoña", "Bagoñez", "77232", "20/12/2025", "23/12/2025", "6653"),
-      new Viajero("Bogoña", "Bogoñez", "5532", "20/12/2026", "23/12/2026", "323")
+      // new Viajero("Bogoña", "Bogoñez", "5532", "20/12/2026", "23/12/2026", "323")
     ];
   }
+
+  mostrarNuevoFormulario(){
+    this.nuevo = true;
+    this.editar = false;
+  }
+
+  aceptar(){
+    if(this.editar==false){
+      this.crearViajero()
+    }else{
+      this.updateViajero();
+    }
+  }
+
+  crearViajero(){
+    this.viajero = new Viajero(this.nombreInt, this.apellidoInt, this.dniInt, this.fechaIngresoInt, this.fechaSalidaInt, this.habitacionInt)
+    this.viajeros.push(this.viajero);
+    this.nuevo = false;
+  }
+
+  updateViajero(){
+
+  }
+
+  eliminarViajero(){
+    this.nuevo = true;
+    this.editar = true;
+    alert(this.nuevo);
+    this.posicion = parseInt(event);
+  }
+
+  modificarViajero(){
+
+  }
+
 }
